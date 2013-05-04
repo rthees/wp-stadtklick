5e9e05e0fc5b14d3c1316684ddc0bfbe428c9362
unknown <rthees@.main-post.intern>
2013-05-04 10:39:04 +0200

    clean 2

--- stadtklick.php
+++ stadtklick.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name: Stadtklick
  Version: 0.1
- Plugin URI: http://herrthees.de/2012/09/15/wordpress-plugin-a-year-before/
+ Plugin URI: http://herrthees.de/
  Author: Ralf Thees
  Author URI: http://herrthees.de/
  Description: Plugin für "Lass den Klick in deiner Stadt"
