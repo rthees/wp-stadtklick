46f6a9ff927c40570b84e0527a5914ffb373c131
Ralf Thees <ralf.thees@gmail.com>
2013-05-04 10:55:47 +0200

    r5

--- wp-stadtklick.php
+++ wp-stadtklick.php
@@ -8,4 +8,15 @@
  Description: Plugin für "Lass den Klick in deiner Stadt"
  */
  
+ if (!defined('WP_CONTENT_URL'))
+	define('WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
+if (!defined('WP_CONTENT_DIR'))
+	define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+if (!defined('WP_PLUGIN_URL'))
+	define('WP_PLUGIN_URL', WP_CONTENT_URL . '/plugins');
+if (!defined('WP_PLUGIN_DIR'))
+	define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
+if (!defined('WP_LANG_DIR'))
+	define('WP_LANG_DIR', WP_CONTENT_DIR . '/languages');
+ 
 ?>
