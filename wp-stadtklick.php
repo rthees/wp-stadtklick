ebbf08162d9d86be3e3dda05ac7a242c1644b98c
Ralf Thees <ralf.thees@gmail.com>
2013-05-20 14:59:29 +0200

    shop-name-options works

--- wp-stadtklick.php
+++ wp-stadtklick.php
@@ -8,6 +8,8 @@
  Description: Plugin für "Lass den Klick in deiner Stadt"
  */
  
+load_plugin_textdomain('wp_stadtklick', false, dirname(plugin_basename(__FILE__)) . '');
+//include_once dirname( __FILE__ )  .'/wp_stadtklick_options.php';
  
  $wuebu_stadtklick_buchhandlungUrlArray=  Array(
  	array(
@@ -46,7 +48,7 @@
 			$isbn="978".$isbn;  // Buchkennung
 			$isbn=substr($isbn,0,strlen($isbn)-1); // ISBN-10-Prüfziffer abschneiden
 			$pruefziffer=(10-(($isbn[0]+$isbn[2]+$isbn[4]+$isbn[6]+$isbn[8]+$isbn[10]+3*($isbn[1]+$isbn[3]+$isbn[5]+$isbn[7]+$isbn[9]+$isbn[11]))%10)%10); // Prüfziffer berechnen
-			$isbn=$isbn.$pruefziffer;
+			$isbn=$isbn.$pruefziffer; // Prüfziffer anhängen
 			
 		}
 		return $isbn;
@@ -91,4 +93,79 @@
 	add_shortcode("stadtklick", "wuebu_stadtklick_shortcode_func");
 
 
+// Options
+
+
+add_action('admin_menu', 'wp_stadtklick_create_menu');
+function wp_stadtklick_create_menu() {
+	add_options_page('WP-Stadtklick-Einstellungen', 'WP-Stadtklick', 'manage_options', 'wp_stadtklick', 'wp_stadtklick_options_page');
+}
+
+
+add_action('admin_init', 'register_wp_stadtklick' );
+function register_wp_stadtklick() {
+	register_setting( 'wp_stadtklick_options', 'wp_stadtklick_options', 'wp_stadtklick_validate' );
+	add_settings_section('wp_stadtklick_shops', 'Webshop-Einstellungen', 'wp_stadtklick_section_shop', 'wp_stadtklick');
+	add_settings_field('wp_stadtklick_shop','Name des Buchladens', 'wp_stadtklick_shop_name_input', 'wp_stadtklick', 'wp_stadtklick_shops');
+}
+
+function wp_stadtklick_section_shop() {
+	echo '<p>Webshop-EInstellungen</p>';
+}
+
+function wp_stadtklick_shop_name_input() {
+	$options = get_option('wp_stadtklick_options');
+	echo "<pre>"; print_r($options); echo "</pre>";
+	$i=0;
+	if (is_array($options)) {
+	foreach ($options as $o) {
+		echo "<input id='wp_stadtklick_shop_$i' name='wp_stadtklick_options[$i][shop][name]' size='80' type='text' value='".$o['shop']['name']."' /><br/>";
+		$i++;
+	}
+	}
+			echo "<input id='wp_stadtklick_shop' name='wp_stadtklick_options[$i][shop][name]' size='80' type='text' value='' />";
+	
+}
+
+// validate  options
+function wp_stadtklick_validate($input) {
+	$options = get_option('wp_stadtklick_options');
+	$final=array();
+	foreach($input as $o) {
+		$o['shop']['name']=trim($o['shop']['name']);
+		if ($o['shop']['name']!=='') $final[]=$o ;
+	}
+	$options=$final;
+	return $options;
+}
+
+
+
+function wp_stadtklick_options_page() { 
+	?>
+	<div class="wrap">
+	<h2>WP-Stadtklick - Einstellungen</h2>
+	<form method="post" action="options.php"> 
+		
+	<?php settings_fields( 'wp_stadtklick_options' ); ?>
+	
+	<?php // $options = get_option('wp_stadtklick'); print_r($options); ?>
+	<?php do_settings_sections('wp_stadtklick'); ?>
+	<!--
+	<table class="form-table">
+		
+	    		<tr><th colspan="2">Neue Buchhandlung</th></tr>
+	    		<tr valign="top"><th scope="row"><?php __('Name der Buchhandlung','wp_stadtklick'); ?></th>
+	                    <td><input name="wp_stadtklick" type="text" value="" /></td>
+	                </tr>
+	                <tr valign="top"><th scope="row"><?php __('URL zu einem Buch im Webshop (%%isbn%% anstelle der ISBN-Nummer','wp_stadtklick'); ?></th>
+	                    <td><input type="text" name="wp_stadtklick2" value="" /></td>
+	                </tr>
+	            </table>
+	           //-->
+				<?php submit_button(); ?>
+	</form>
+	</div>
+<?php 
+} 
 ?>
